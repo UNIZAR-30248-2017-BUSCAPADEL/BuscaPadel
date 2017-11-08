08d9d40854e7c79b0ecbccb30f2efce4d86cf8ea
Iván Santamaría <ivansantamaria.m@gmail.com>
2017-11-08 23:34:19 +0100

    Creación pantalla crear partido y validación formulario a medias

--- app/src/main/java/company/buscapadel/MainActivity.java
+++ app/src/main/java/company/buscapadel/MainActivity.java
@@ -1,13 +1,75 @@
 package company.buscapadel;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView title;
+    private EditText lugar;
+    private EditText fecha;
+    private EditText hora;
+    private Button crearButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final company.buscapadel.MainActivity local = this;
+        lugar = (EditText) findViewById(R.id.editText);
+        fecha = (EditText) findViewById(R.id.editText3);
+        hora = (EditText) findViewById(R.id.editText4);
+        crearButton = (Button) findViewById(R.id.button);
+
+        crearButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View view) {
+                String fechaText = fecha.getText().toString();
+                String horaText = hora.getText().toString();
+                String lugarText = lugar.getText().toString();
+
+                if(fechaText.equals("") || horaText.equals("") || lugarText.equals(""))
+                {
+                    AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(local);
+
+                    dlgAlert.setMessage("Todos estos campos son obligatorios");
+                    dlgAlert.setTitle("Error...");
+                    dlgAlert.setPositiveButton("OK", null);
+                    dlgAlert.setCancelable(true);
+                    dlgAlert.create().show();
+
+                    dlgAlert.setPositiveButton("Ok",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            });
+                } else {
+                    AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(local);
+
+                    dlgAlert.setMessage("Lugar: " + lugarText + " Fecha: " + fechaText +
+                            " Hora: " + horaText);
+                    dlgAlert.setTitle("Partido Creado");
+                    dlgAlert.setPositiveButton("OK", null);
+                    dlgAlert.setCancelable(true);
+                    dlgAlert.create().show();
+                    //ACTUALIZAR BASE DE DATOS
+//                    Intent i = new Intent(local, Modificar_Perfil_2.class);
+//                    startActivityForResult(i, 0);
+                }
+            }
+        });
+
+
     }
 }
