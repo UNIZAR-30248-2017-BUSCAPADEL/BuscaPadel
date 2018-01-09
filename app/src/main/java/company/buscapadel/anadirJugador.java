ead0d5fd82a89d728e162f3d90edb62bc7b8f5f0
Iván Santamaría <ivansantamaria.m@gmail.com>
2018-01-10 00:18:01 +0100

    validacion datos pantalla añadir jugadores a liga/torneo

--- app/src/main/java/company/buscapadel/anadirJugador.java
+++ app/src/main/java/company/buscapadel/anadirJugador.java
@@ -1,13 +1,135 @@
 package company.buscapadel;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class anadirJugador extends AppCompatActivity {
+
+    private EditText correoEditText;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_anadir_jugador);
+
+        final company.buscapadel.anadirJugador local = this;
+
+        correoEditText = (EditText) findViewById(R.id.editText13);
+        button = (Button) findViewById(R.id.button13);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                final String correo = correoEditText.getText().toString();
+                if (correo.equals("")) {
+                    AlertDialog.Builder dlgAlert = new AlertDialog.Builder(local);
+
+                    dlgAlert.setMessage("No puede haber campos vacíos");
+                    dlgAlert.setTitle("Error...");
+                    dlgAlert.setPositiveButton("OK", null);
+                    dlgAlert.setCancelable(true);
+                    dlgAlert.create().show();
+
+                    dlgAlert.setPositiveButton("Ok",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            });
+                } else if (1 == 0) { //correo no encontrado en base de datos
+                    AlertDialog.Builder dlgAlert = new AlertDialog.Builder(local);
+
+                    dlgAlert.setMessage("El correo no existe");
+                    dlgAlert.setTitle("Error...");
+                    dlgAlert.setPositiveButton("OK", null);
+                    dlgAlert.setCancelable(true);
+                    dlgAlert.create().show();
+
+                    dlgAlert.setPositiveButton("Ok",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            });
+                } else {
+                    //BASE DE DATOS
+                    /*JugadoresDAO jugadoresDAO = new JugadoresDAO();
+                    jugadoresDAO.getJugadorRegistro(correoText, new ServerCallBack() {
+                        @Override
+                        public void onSuccess(JSONArray result) {
+                            if (result.length()!=0){
+                                try {
+                                    JSONObject jsonObject = result.getJSONObject(0);
+                                    String contrasena = (String) jsonObject.get("contrasena");
+                                    int id = (int) jsonObject.get("id");
+                                    if (contrasena.equals(passwordText)) {
+                                        restoApp(correoText, id);
+                                    } else {
+
+                                        ((EditText) findViewById(R.id.editText8)).getText().clear();
+                                        AlertDialog.Builder dlgAlert = new AlertDialog.Builder(local);
+
+                                        dlgAlert.setMessage("El correo o la contraseña son incorrectos");
+                                        dlgAlert.setTitle("Error...");
+                                        dlgAlert.setPositiveButton("OK", null);
+                                        dlgAlert.setCancelable(true);
+                                        dlgAlert.create().show();
+
+                                        dlgAlert.setPositiveButton("Ok",
+                                                new DialogInterface.OnClickListener() {
+                                                    public void onClick(DialogInterface dialog, int which) {
+
+                                                    }
+                                                });
+                                    }
+                                } catch (Exception e) {
+                                    Log.d("Error:", e.toString());
+                                }
+                            }
+                            else {
+                                ((EditText) findViewById(R.id.editText8)).getText().clear();
+                                AlertDialog.Builder dlgAlert = new AlertDialog.Builder(local);
+
+                                dlgAlert.setMessage("El correo no ha sido registrado");
+                                dlgAlert.setTitle("Error...");
+                                dlgAlert.setPositiveButton("OK", null);
+                                dlgAlert.setCancelable(true);
+                                dlgAlert.create().show();
+
+                                dlgAlert.setPositiveButton("Ok",
+                                        new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int which) {
+
+                                            }
+                                        });
+                            }
+                        }
+
+                        @Override
+                        public void onError() {
+                            AlertDialog.Builder dlgAlert = new AlertDialog.Builder(local);
+
+                            dlgAlert.setMessage("Problema con la base de datos, inténtelo más tarde");
+                            dlgAlert.setTitle("Error...");
+                            dlgAlert.setPositiveButton("OK", null);
+                            dlgAlert.setCancelable(true);
+                            dlgAlert.create().show();
+
+                            dlgAlert.setPositiveButton("Ok",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int which) {
+
+                                        }
+                                    });
+                        }
+                    }, true);*/
+                }
+            }
+        });
     }
 }
